dddce5eadcd54b729d5ea9988f29783dfa246233
Andrey Kondratov <22258361+andead@users.noreply.github.com>
2020-06-25 23:23:17 +0300

    Created the projects.

---
+++ src/PillsBot/PillsBot.Server/Program.cs
@@ -0,0 +1,57 @@
+using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace PillsBot.Server
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            Log.Logger = CreateLoggerConfiguration().CreateLogger();
+
+            try
+            {
+                Log.Information("Starting host");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static LoggerConfiguration CreateLoggerConfiguration()
+        {
+            LoggerConfiguration configuration = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("Environment", Env.Name)
+                .Enrich.WithProperty("Version", new
+                {
+                    Server = typeof(Program).Assembly.GetName().Version.ToString(3)
+                }, true);
+
+            configuration = Env.IsDevelopment
+                ? configuration.MinimumLevel.Debug()
+                : configuration.MinimumLevel.Information();
+
+            return configuration.WriteTo.Console();
+        }
+
+        public static IWebHostBuilder CreateHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder<Startup>(args)
+                .UseKestrel(options => options.ListenAnyIP(Env.Port))
+                .UseSerilog();
+        }
+    }
+}
---
+++ src/PillsBot/PillsBot.Server/Startup.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace PillsBot.Server
+{
+    public class Startup
+    {
+        public Startup(IHostEnvironment environment)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{Env.Name}.json", true, true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // TODO add the services
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSerilogRequestLogging();
+            // TODO add the webhook middleware
+        }
+    }
+}
